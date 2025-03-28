4daefd72e6129b3fb23d3305053a3fc9345dcb66
denis-spe <denisbrian07@gmail.com>
2025-03-29 01:20:27 +0200

    Update and debugging model for tpu

--- src/exttorch/models.py
+++ src/exttorch/models.py
@@ -1,4 +1,4 @@
-""" Praise Ye The Lord Your God """
+"""Praise Ye The Lord Your God"""
 
 # Import libraries
 from torch import nn as __nn__
@@ -62,7 +62,7 @@ class Sequential(__nn__.Module):
         {'val_loss': ..., 'val_accuracy': ...}
         """
         super(Sequential, self).__init__()
-        
+
         self.loss = None
         self.optimizer = None
         self.layers = layers if layers else []
@@ -71,23 +71,23 @@ class Sequential(__nn__.Module):
         self.__progbar = None
         self.stop_training = False
         self.__ENV = __ENV__
-    
+
     def get_weights(self):
         return self.__model.state_dict()
-    
+
     def set_weights(self, weight):
         self.__model.load_state_dict(weight)
 
     def add(self, layer: __nn__.Module):
         self.layers.append(layer)
-        
+
     def __handle_callbacks(self, callback_method, logs=None, epoch: int = None):
-    
+
         if self.__callbacks is not None:
             for callback in self.__callbacks:
                 # Set the model and stop_training to the callback
                 callback.model = self
-                
+
                 # Check if the present callback method
                 match callback_method:
                     case "on_train_begin":
@@ -106,7 +106,6 @@ class Sequential(__nn__.Module):
                         callback.on_epoch_begin(epoch)
                     case "on_epoch_end":
                         callback.on_epoch_end(epoch, logs)
-                
 
     def fit(
         self,
@@ -156,7 +155,7 @@ class Sequential(__nn__.Module):
             callbacks: (Optional[List[Callback]])
                 Model list of callbacks.
             nprocs: (int)
-                The number of processes/devices for the replication. 
+                The number of processes/devices for the replication.
                 At the moment, if specified, can be either 1 or the maximum number of devices.
             start_method: (str) "fork" by default
                 The method to start the process.
@@ -168,10 +167,11 @@ class Sequential(__nn__.Module):
         from ._data_handle import DataHandler
         import torch
         from torch import nn as _nn
-        
+        import torch_xla.core.xla_model as xm
+        import torch_xla.distributed.xla_multiprocessing as xmp
+
         self.stop_training = False
-        
-        
+
         # Set the val_batch_size to batch_size if None
         val_batch_size = val_batch_size if val_batch_size is not None else batch_size
 
@@ -181,41 +181,41 @@ class Sequential(__nn__.Module):
         if type(random_seed) == int:
             # Set the random seed
             torch.manual_seed(random_seed)
-            
+
         if callbacks is not None:
             self.__callbacks = callbacks
-        
+
         def training(rank, flag):
-            
-            self.__device = self.__ENV["EXTTORCH_XM"].xla_device() 
-            # if "EXTTORCH_TPU" in self.__ENV 
+
+            self.__device = xm.xla_device()
+            # if "EXTTORCH_TPU" in self.__ENV
             # else (
-            #     "cuda" 
-            #     if torch.cuda.is_available() 
+            #     "cuda"
+            #     if torch.cuda.is_available()
             #     else "cpu"
-            # ))            
+            # ))
 
             # self.__model_list = _nn.ModuleList(self.layers).to(self.__device).float()
-            
+
             # Initialize the model
             self.__model = __nn__.Sequential(*self.layers).to(self.__device).float()
-            
+
             # Instantiate the Loss and optimizer
             self.loss = self.loss()
             self.optimizer = self.optimizer(self.__model.parameters())
-            
+
             if validation_split is not None and validation_data is None:
-                
+
                 # Handle the callbacks on train begin
                 self.__handle_callbacks("on_train_begin")
-                
+
                 print(end="\n")
-                    
+
                 for epoch in range(epochs):
-                    
+
                     # Handle the callbacks on epoch begin
                     self.__handle_callbacks("on_epoch_begin", epoch=epoch)
-                    
+
                     if verbose != 0 and verbose is not None:
                         # Print the epochs
                         print(f"Epoch {epoch + 1}/{epochs}")
@@ -233,7 +233,9 @@ class Sequential(__nn__.Module):
                     )
 
                     # Get the train and validation sample
-                    train_sample, val_sample = data.data_preprocessing(nprocs, val_size=validation_split)
+                    train_sample, val_sample = data.data_preprocessing(
+                        nprocs, val_size=validation_split
+                    )
 
                     # Train the train sample
                     train_metric = self.__train(
@@ -247,8 +249,7 @@ class Sequential(__nn__.Module):
                         show_val_progress=True,
                         **dataloader_kwargs,
                     )
-                    
-                    
+
                     # Add the train metric to the history
                     history.add_history(train_metric)
 
@@ -271,40 +272,44 @@ class Sequential(__nn__.Module):
                     # Update the metrics by adding val_metric.
                     metrics.update(val_metric)
 
-                    if verbose != 0 and verbose is not None:    
+                    if verbose != 0 and verbose is not None:
                         # Show the progress bar on each epoch
-                        self.__progbar.update(self.__train_data_size, val_metric.items(), finalize=True)
+                        self.__progbar.update(
+                            self.__train_data_size, val_metric.items(), finalize=True
+                        )
 
                     # Add the validation metric to the history
                     history.add_history(val_metric)
-                    
+
                     # Make a copy
                     metric_copy = train_metric.copy()
                     metric_copy.update(val_metric)
-                                    
+
                     # Handle the callbacks on epoch end
-                    self.__handle_callbacks("on_epoch_end", logs=metric_copy, epoch=epoch)
-                    
+                    self.__handle_callbacks(
+                        "on_epoch_end", logs=metric_copy, epoch=epoch
+                    )
+
                     if self.stop_training:
                         break
-                    
+
                     if "EXTTORCH_TPU" in self.__ENV:
                         self.__ENV["EXTTORCH_XM"].rendezvous("epoch_sync")
-                    
+
                 # Handle the callbacks on train end
                 self.__handle_callbacks("on_train_end", logs=history.history)
 
             elif validation_data is not None:
-                
+
                 # Handle the callbacks on train begin
                 self.__handle_callbacks("on_train_begin")
-                
+
                 print(end="\n")
-                
+
                 for epoch in range(epochs):
                     # Handle the callbacks on epoch begin
                     self.__handle_callbacks("on_epoch_begin", epoch=epoch)
-                    
+
                     if verbose != 0 and verbose is not None:
                         # Print the epochs
                         print(f"Epoch {epoch + 1}/{epochs}")
@@ -378,39 +383,43 @@ class Sequential(__nn__.Module):
                         **dataloader_kwargs,
                     )
 
-                    if verbose != 0 and verbose is not None:    
+                    if verbose != 0 and verbose is not None:
                         # Show the progress bar on each epoch
-                        self.__progbar.update(self.__train_data_size, val_metric.items(), finalize=True)
+                        self.__progbar.update(
+                            self.__train_data_size, val_metric.items(), finalize=True
+                        )
 
                     # Add the validation metric to the history
                     history.add_history(val_metric)
-                    
+
                     # Make a copy
                     metric_copy = train_metric.copy()
                     metric_copy.update(val_metric)
-                                    
+
                     # Handle the callbacks on epoch end
-                    self.__handle_callbacks("on_epoch_end", logs=metric_copy, epoch=epoch)
-                    
+                    self.__handle_callbacks(
+                        "on_epoch_end", logs=metric_copy, epoch=epoch
+                    )
+
                     if self.stop_training:
                         break
-                    
+
                     if "EXTTORCH_TPU" in self.__ENV:
                         self.__ENV["EXTTORCH_XM"].rendezvous("epoch_sync")
-                
+
                 # Handle the callbacks on train end
                 self.__handle_callbacks("on_train_end", logs=history.history)
 
-            else:            
+            else:
                 # Handle the callbacks on train begin
                 self.__handle_callbacks("on_train_begin")
-                
+
                 print(end="\n")
-                
+
                 for epoch in range(epochs):
                     # Handle the callbacks on epoch begin
                     self.__handle_callbacks("on_epoch_begin", epoch=epoch)
-                    
+
                     if verbose != 0 and verbose is not None:
                         # Print the epochs
                         print(f"Epoch {epoch + 1}/{epochs}")
@@ -438,40 +447,37 @@ class Sequential(__nn__.Module):
                         nprocs=nprocs,
                         **dataloader_kwargs,
                     )
-                    
 
                     # Add the train metric to the history
                     history.add_history(train_metric)
-                    
+
                     print(end="\n")
-                    
+
                     # Handle the callbacks on epoch end
-                    self.__handle_callbacks("on_epoch_end", epoch=epoch, logs=train_metric)
-                    
+                    self.__handle_callbacks(
+                        "on_epoch_end", epoch=epoch, logs=train_metric
+                    )
+
                     if self.stop_training:
                         break
-                    
+
                     if "EXTTORCH_TPU" in self.__ENV:
                         self.__ENV["EXTTORCH_XM"].rendezvous("epoch_sync")
 
                 # Handle the callbacks on train end
                 self.__handle_callbacks("on_train_end", logs=history.history)
-                
+
         # if "EXTTORCH_TPU" in self.__ENV:
         #     if nprocs == 1:
-                # self.__ENV["EXTTORCH_XMP"].spawn(
-                #     training, args=(None,), 
-                #     nprocs=nprocs, 
-                #     start_method=start_method)
+        # self.__ENV["EXTTORCH_XMP"].spawn(
+        #     training, args=(None,),
+        #     nprocs=nprocs,
+        #     start_method=start_method)
         #     else:
         #         pass
         # else:
         # training(None, None)
-        self.__ENV["EXTTORCH_XMP"].spawn(
-                    training, args=(None,), 
-                    nprocs=nprocs, 
-                    start_method=start_method
-                    )
+        xmp.spawn(training, args=(None,), nprocs=nprocs, start_method=start_method)
         return history
 
     def predict_proba(self, X):
@@ -569,10 +575,8 @@ class Sequential(__nn__.Module):
 
         # Create the list for metric
         metric_storage = MetricStorage(
-            self.__device, 
-            self.metrics, 
-            batch_size=batch_size
-            )
+            self.__device, self.metrics, batch_size=batch_size
+        )
 
         # Indicate the model to train
         self.__model.train()
@@ -596,16 +600,16 @@ class Sequential(__nn__.Module):
         #     self.__progbar = Progbar(len(data), verbose=verbose)
 
         # # Handle on batch begin callback
-        self.__handle_callbacks('on_batch_begin')
-        
+        self.__handle_callbacks("on_batch_begin")
+
         # Loop over the data
         for idx, (feature, label) in enumerate(data):
-            
+
             feature, label = feature.to(self.__device), label.to(self.__device)
-            
+
             # Zero the gradient.
             self.optimizer.zero_grad()
-            
+
             # Make prediction
             predict = self.__model(feature.float())
 
@@ -620,12 +624,12 @@ class Sequential(__nn__.Module):
 
             # Compute the gradient
             loss.backward()
-            
+
             # Measurement live update
             measurements = metric_storage.measurements_compiler()
-            
+
             print(measurements)
-            
+
             # if verbose is not None:
             #     if show_val_progress:
             #         if idx < len(data) - 1:
@@ -633,21 +637,21 @@ class Sequential(__nn__.Module):
             #     else:
             #         # Update the progress bar
             #         self.__progbar.update(idx + 1, measurements)
-                                        
+
             # update the parameters
             # if "EXTTORCH_TPU" in self.__ENV:
-                # self.__ENV["EXTTORCH_XM"].optimizer_step(self.optimizer)
+            # self.__ENV["EXTTORCH_XM"].optimizer_step(self.optimizer)
             self.optimizer.step()
             xm.mark_step()
             # else:
             #     self.optimizer.step()
-                
+
         # Measurements
-        measurements = metric_storage.measurements    
-        
+        measurements = metric_storage.measurements
+
         # Handle on batch begin callback
-        self.__handle_callbacks('on_batch_end', logs=measurements)
-                
+        self.__handle_callbacks("on_batch_end", logs=measurements)
+
         return measurements
 
     def evaluate(
@@ -696,10 +700,8 @@ class Sequential(__nn__.Module):
 
         # Create the list for metric
         metric_storage = MetricStorage(
-            self.__device, 
-            metrics=self.metrics, 
-            batch_size=batch_size, 
-            train=False)
+            self.__device, metrics=self.metrics, batch_size=batch_size, train=False
+        )
 
         # Indicate the model to evaluate
         self.__model.eval()
@@ -718,13 +720,13 @@ class Sequential(__nn__.Module):
 
         # # Declare the progbar
         # progbar = None
-        
+
         steps = 0
 
         if verbose:
             # Instantiate the progress bar
             progbar = Progbar(len(data), verbose=verbose)
-        
+
         # Handle on validation begin
         self.__handle_callbacks("on_validation_begin")
 
@@ -757,24 +759,24 @@ class Sequential(__nn__.Module):
 
                 # Add the prediction, labels(target) and loss to metric storage
                 metric_storage.add_metric(predict, label=target, loss=loss.item())
-                
+
                 # Measurement live update
                 live_measurements = metric_storage.measurements_compiler()
-                                
+
                 if verbose is not None:
                     if 0.0 not in list(dict(live_measurements).values()):
                         # Update the progress bar
                         progbar.update(idx + 1, live_measurements)
-                    
+
                 if "EXTTORCH_TPU" in self.__ENV:
                     self.__ENV["EXTTORCH_XM"].mark_step()
-        
+
         # Final measurements
         measurements = metric_storage.measurements
 
         # Handle on validation end
         self.__handle_callbacks("on_validation_end", logs=measurements)
-            
+
         return measurements
 
     def compile(
@@ -813,24 +815,21 @@ class Sequential(__nn__.Module):
                     - AUC: Area under the curve for classification problem.
                     - Recall: A classification metric for measuring model recall score.
                     - Precision: A classification metric for measuring model precision score.
-                
+
         """
         # Import libraries
         from ._metrics_handles import str_val_to_metric
 
         self.optimizer = (
-            optimizer 
-            if isinstance(optimizer, __Optimizer__) 
+            optimizer
+            if isinstance(optimizer, __Optimizer__)
             else __change_str_to_optimizer__(optimizer)
         )
-        self.loss = (
-            loss 
-            if isinstance(loss, __Loss__) 
-            else __change_str_to_loss__(loss)
-        )
+        self.loss = loss if isinstance(loss, __Loss__) else __change_str_to_loss__(loss)
         self.metrics = str_val_to_metric(metrics) if metrics is not None else []
 
 
 if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
