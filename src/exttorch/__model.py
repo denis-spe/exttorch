6a428f0daf43c993cf0bb767338959440bae9ff0
denis-spe <56014815+denis-spe@users.noreply.github.com>
2025-09-07 23:12:08 +0000

    TODO: evaluate method

--- src/exttorch/__model.py
+++ src/exttorch/__model.py
@@ -2,6 +2,8 @@
 
 # Import libraries.
 import torch
+import numpy as np
+from torch.nn.functional import softmax
 import torch.nn as nn
 from typing import List, Optional
 from src.exttorch.history import History
@@ -14,6 +16,9 @@ from src.exttorch.__types import (
     VerboseType, FillStyleType, EmptyStyleType, ProgressType, Weight
 )
 from src.exttorch.utils import ProgressBar
+from src.exttorch.losses import Loss
+from src.exttorch.optimizers import Optimizer
+from typing import List, Any
 
 class ModelFit:
     # Constructor
@@ -36,7 +41,7 @@ class ModelFit:
         self.model: nn.Module | None = None
         self._device: torch.device | str = torch.device("cpu")
         self.__callbacks: List | None = None
-
+    
     # Private methods
     def __handle_callbacks(
         self, callback_method: str, logs=None, epoch: int | None = None
@@ -387,9 +392,265 @@ class ModelFit:
                 )
 
         return self.val_metric_storage.measurements
- 
 
-class Model(ModelFit):
+class ModelCompilation:
+    def compile(
+        self,
+        optimizer: Optimizer | str,
+        loss: Loss | str,
+        metrics: List[str | Metric] | None = None,
+    ):
+        """
+        Compile the model.
+
+        Parameters
+        ----------
+            optimizer : (Optimizer | str)
+                For updating the model parameters.
+                Here are some of the options:
+                    - Adam: Adam optimizer
+                    - SGD: Stochastic Gradient Descent
+                    - RMSprop: RMSprop optimizer
+                    - Adadelta: Adadelta optimizer
+            loss : (Loss | str)
+                Measures model's performance.
+                Here are some of the options:
+                    - BCELoss: Binary Cross Entropy Loss
+                    - BCEWithLogitsLoss: Binary Cross Entropy Loss with Logits
+                    - CrossEntropyLoss: Cross Entropy Loss
+                    - MSELoss: Mean Squared Error Loss
+                    - NLLLoss: Negative Log Likelihood Loss
+            metrics : (Optional[List[Metric|str]]) default
+                Measures model's performance.
+                Here are some of the options:
+                    - Accuracy: A classification metric for measuring model accuracy.
+                    - F1Score: A classification metric for measuring model f1 score.
+                    - MAE: Mean absolute error for regression problem.
+                    - MSE: Mean squared error for regression problem.
+                    - AUC: Area under the curve for classification problem.
+                    - Recall: A classification metric for measuring model recall score.
+                    - Precision: A classification metric for measuring model precision score.
+
+        """
+        self.optimizer_obj = (
+            optimizer
+            if type(optimizer) != str
+            else self.__change_str_to_optimizer__(optimizer)
+        )
+        self.loss_obj = loss if type(loss) != str else self.__change_str_to_loss__(loss)
+        self.metrics = (
+            self.__str_val_to_metric__(metrics) if metrics is not None else []
+        )
+
+    @staticmethod
+    def __str_val_to_metric__(
+        metric_list: List[Any],
+    ) -> List[Metric]:
+        from src.exttorch.metrics import (
+            Accuracy,
+            MeanSquaredError,
+            R2,
+            MeanAbsoluteError,
+            Recall,
+            Precision,
+            Jaccard,
+            Auc,
+            MatthewsCorrcoef,
+            ZeroOneLoss,
+            TopKAccuracy,
+            F1Score,
+        )
+
+        new_metric_list: List[Metric] = []
+        for new_metric_name in metric_list:
+            if type(new_metric_name) == str:
+                match new_metric_name:
+
+                    case "acc" | "Acc" | "accuracy" | "Accuracy":
+                        new_metric_list.append(Accuracy(new_metric_name))
+                    case "mse" | "MSE" | "MeanSquaredError":
+                        new_metric_list.append(MeanSquaredError(new_metric_name))
+                    case "r2" | "R2":
+                        new_metric_list.append(R2(new_metric_name))
+                    case "mae" | "MAE" | "MeanAbsoluteError":
+                        new_metric_list.append(MeanAbsoluteError(new_metric_name))
+                    case "recall" | "rec" | "Recall":
+                        new_metric_list.append(Recall(new_metric_name))
+                    case "precision" | "pre" | "Precision":
+                        new_metric_list.append(Precision(new_metric_name))
+                    case "jaccard" | "jac" | "Jaccard":
+                        new_metric_list.append(Jaccard(new_metric_name))
+                    case "Auc" | "auc":
+                        new_metric_list.append(Auc(new_metric_name))
+                    case "MatthewsCorrcoef" | "mat" | "mc" | "MC":
+                        new_metric_list.append(MatthewsCorrcoef(new_metric_name))
+                    case "ZeroOneLoss" | "zero" | "zol":
+                        new_metric_list.append(ZeroOneLoss(new_metric_name))
+                    case "TopKAccuracy" | "TKA" | "tka":
+                        new_metric_list.append(TopKAccuracy(new_metric_name))
+                    case "F1Score" | "f1" | "f1score" | "F1" | "f1_score":
+                        new_metric_list.append(F1Score(new_metric_name))
+                    case _:
+                        raise ValueError(f"Unknown metric name `{new_metric_name}`")
+            else:
+                new_metric_list.append(new_metric_name)
+
+        return new_metric_list
+
+    @staticmethod
+    def __change_str_to_loss__(loss: str):
+        from src.exttorch.losses import (
+            MSELoss,
+            L1Loss,
+            NLLLoss,
+            CrossEntropyLoss,
+            BCELoss,
+            BCEWithLogitsLoss,
+            MarginRankingLoss,
+        )
+
+        match loss:
+            case "MSELoss" | "mse" | "mean_squared_error" | "MSE":
+                return MSELoss()
+            case "L1Loss" | "l1" | "mean_absolute_error" | "MAE":
+                return L1Loss()
+            case "NLLLoss" | "nll" | "negative_log_likelihood" | "nll_loss":
+                return NLLLoss()
+            case (
+                "CrossEntropyLoss"
+                | "cross_entropy"
+                | "crossentropy"
+                | "categorical_crossentropy"
+            ):
+                return CrossEntropyLoss()
+            case "BCELoss" | "bce" | "binary_crossentropy":
+                return BCELoss()
+            case (
+                "BCEWithLogitsLoss"
+                | "bce_with_logits"
+                | "binary_cross_entropy_with_logits"
+            ):
+                return BCEWithLogitsLoss()
+            case "MarginRankingLoss" | "margin_ranking":
+                return MarginRankingLoss()
+            case _:
+                raise ValueError(
+                    "Invalid loss name. Available options: "
+                    "MSELoss, L1Loss, NLLLoss, CrossEntropyLoss, "
+                )
+
+    @staticmethod
+    def __change_str_to_optimizer__(optimizer: str):
+        from src.exttorch.optimizers import (
+            Adam,
+            SGD,
+            RMSprop,
+            Adadelta,
+            Adagrad,
+            Adamax,
+            ASGD,
+        )
+
+        match optimizer:
+            case "Adam" | "adam":
+                return Adam()
+            case "SGD" | "sgd":
+                return SGD()
+            case "RMSprop" | "rmsprop":
+                return RMSprop()
+            case "Adadelta" | "adadelta":
+                return Adadelta()
+            case "Adagrad" | "adagrad":
+                return Adagrad()
+            case "Adamax" | "adamax":
+                return Adamax()
+            case "ASGD" | "asgd":
+                return ASGD()
+            case _:
+                raise ValueError(
+                    f"Invalid optimizer name `{optimizer}`. Available options: "
+                    "Adam, SGD, RMSprop, Adadelta, Adagrad, Adamax, ASGD."
+                )
+
+class ModelPrediction:
+    def __init__(self):
+        self._device: torch.device | str = torch.device("cpu")
+        self.loss_obj = None
+        self.model: nn.Module | None = None
+        self._progressbar: ProgressBar | None = None
+        self._verbose: VerboseType = "full"
+        self._progress_bar_width: int = 40
+        self._progress_fill_style: FillStyleType = "━"
+        self._progress_empty_style: EmptyStyleType = "━"
+        self._progress_fill_color: str = "\033[92m"
+        self._progress_empty_color: str = "\033[90m"
+        self._progress_percentage_colors: List[str] | None = None
+        self._progress_progress_type: ProgressType = "bar"
+        
+    # Public methods
+    def predict_proba(self, x, verbose=None):
+        x = (x.float() if type(x) == torch.Tensor else torch.tensor(x).float()).to(
+            self._device
+        )
+        
+        if self.model is None:
+            raise ValueError("The model is not trained yet. Please train the model first.")
+
+        # Instantiate the progress bar
+        progressbar = ProgressBar(
+            bar_width=self._progress_bar_width,
+            fill_style=self._progress_fill_style,
+            empty_style=self._progress_empty_style,
+            fill_color=self._progress_fill_color,
+            empty_color=self._progress_empty_color,
+            percentage_colors=self._progress_percentage_colors,
+            progress_type=self._progress_progress_type,
+            verbose=self._verbose if verbose is None else verbose,
+        )
+        # Set the progress bar total
+        progressbar.total = len(x)
+
+        # Empty list for probability
+        probability = []
+
+        with torch.no_grad():
+            for i, data in enumerate(x):
+
+                # Make prediction and get probabilities
+                proba = self.model(data)
+
+                # Append the probabilities to the list
+                probability.append(proba.detach().reshape(1, -1).tolist()[0])
+
+                # Update the progress bar
+                progressbar.update(i + 1)
+
+        if type(self.loss_obj).__name__ == "CrossEntropyLoss":
+            probability = softmax(torch.tensor(probability), dim=1).numpy()
+        else:
+            # Convert the probability to numpy array
+            probability = np.array(probability).reshape(-1, 1)
+
+        print("\n")
+
+        return probability
+
+    def predict(self, x, verbose=None):
+
+        # Get the probabilities of x
+        probability = self.predict_proba(x, verbose=verbose)
+
+        # Get the class label if using CrossEntropyLoss
+        # or BCELoss or BCEWithLogitsLoss
+        if type(self.loss_obj).__name__ == "CrossEntropyLoss":
+            predict = probability.argmax(axis=1).reshape(-1, 1)
+        elif type(self.loss_obj).__name__ in ["BCELoss", "BCEWithLogitsLoss"]:
+            predict = probability.round().reshape(-1, 1)
+        else:
+            predict = probability.reshape(-1, 1)
+        return predict
+
+class Model(ModelFit, ModelCompilation, ModelPrediction):
     def __init__(
         self,
     ):
@@ -432,3 +693,7 @@ class Model(ModelFit):
             torch.save(weights, filepath)
         else:
             raise ValueError("Filepath must end with .ext or .we")
+    
+    def evaluate(self, x, y=None, verbose: str | None = None): pass
+        # TODO: implement evaluate method
+        
